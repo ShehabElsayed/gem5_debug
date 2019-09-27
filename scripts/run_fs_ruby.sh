7d2a30bb34138acd1d68bfbe8f06d6092266c342
Shehab Elsayed <shehabyomn@gmail.com>
2019-09-27 15:45:36 -0400

    sim: Added some debug flags to debug the load instruction already executed assertion failure with DerivO3X86 model
    Change-Id: I13aeba5944acbb0d4d23337fe7bbb39a353b4929

--- scripts/run_fs_ruby.sh
+++ scripts/run_fs_ruby.sh
@@ -14,4 +14,9 @@ cp fs_stuff/rcs/testing_hello_template.rcS fs_stuff/rcs/testing_hello.rcS
 sed -i "s/_NUM_THREADS_/${NUM_THREADS}/g" fs_stuff/rcs/testing_hello.rcS
 
 # Run gem5
-sudo ./build/X86_MOESI_hammer/gem5.opt configs/myconfigs/run_fs_ruby.py --script="fs_stuff/rcs/testing_hello.rcS" --num_cpus ${NUM_THREADS} --l2_size='256kB'
+sudo ./build/X86_MOESI_hammer/gem5.opt \
+	--debug-flags=LSQUnit,LSQ,Commit,ROB,IEW,IQ,Decode,Fault,MemDepUnit \
+	--debug-start=13800000000000000 \
+	configs/myconfigs/run_fs_ruby.py \
+	--script="fs_stuff/rcs/testing_hello.rcS" \
+	--num_cpus ${NUM_THREADS}
